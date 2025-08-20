d9e6e73890e7bafb1dee571433e2961c1d34eafe
Mihalhayail <mihalhayail85@gmail.com>
2025-08-21 02:17:48 +0700

    modif chatbot tidio sementara

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,35 +1,23 @@
-// components/Intelliticks.js
-"use client"
+// components/Chat.js
+"use client";
 import { useEffect } from "react";
 
-// Component for Intelliticks chat widget
 const Chat = () => {
-	useEffect(() => {
-		// Paste the Intelliticks script here
-		(function (I, L, T, i, c, k, s) {
-			if (I.iticks) return;
-			I.iticks = { host: c, settings: s, clientId: k, cdn: L, queue: [] };
-			var h = T.head || T.documentElement;
-			var e = T.createElement(i);
-			var l = I.location;
-			e.async = true;
-			e.src = (L || c) + "/client/inject-v2.min.js";
-			h.insertBefore(e, h.firstChild);
-			I.iticks.call = function (a, b) {
-				I.iticks.queue.push([a, b]);
-			};
-		})(
-			window,
-			"https://cdn-v1.intelliticks.com/prod/common",
-			document,
-			"script",
-			"https://app.intelliticks.com",
-			"u3cWw3sDpdKexqXCY_c",
-			{}
-		);
-	}, []);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "//code.tidio.co/xgmpbbvimqe7gfesistmr9ohm47gcvk7.js";
+    script.async = true;
+    document.body.appendChild(script);
 
-	return null;
+    return () => {
+      // Bersihkan script jika komponen di-unmount
+      if (script && document.body.contains(script)) {
+        document.body.removeChild(script);
+      }
+    };
+  }, []);
+
+  return null;
 };
 
 export default Chat;
