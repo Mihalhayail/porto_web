6de3d423c90ff80177e300fef61e781812dd7723
Mihalhayail <mihalhayail85@gmail.com>
2025-08-26 05:25:40 +0700

    add postcard dan post-card/index

--- app/blog/components/PostCard.jsx
+++ app/blog/components/PostCard.jsx
@@ -4,11 +4,17 @@ import { motion } from "framer-motion";
 import PropTypes from "prop-types";
 import BlurImage from "@/public/image/placeholder/blur.jpg";
 
-export default function ProjectCard({ project, index, activeCategory }) {
+import Link from "next/link";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import PropTypes from "prop-types";
+import BlurImage from "@/public/image/placeholder/blur.jpg";
+
+export default function PostCard({ post, index, activeStatus }) {
   return (
     <>
-      {project.category.includes(parseInt(activeCategory)) && (
-        <Link href={"projects/" + project.slug} key={index}>
+      {post.status.includes(parseInt(activeStatus)) && (
+        <Link href={"blog/" + post.slug} key={index}>
           <motion.div
             className="z-10 relative flex justify-center items-start flex-col mb-5 md:px-10 w-full h-auto bg-gray-400 group/tes py-20 px-5 md:py-2 aspect-video "
             initial={{
@@ -24,8 +30,8 @@ export default function ProjectCard({ project, index, activeCategory }) {
             }}
           >
             <Image
-              src={project.thumbnail}
-              alt="Mihalhayail"
+              src={post.thumbnail}
+              alt="Alvalens"
               layout="fill"
               objectFit="cover"
               placeholder="blur"
@@ -33,17 +39,17 @@ export default function ProjectCard({ project, index, activeCategory }) {
               blurDataURL={BlurImage.src}
             />
             <div className="absolute top-0 left-0 bg-gray-600 px-4 py-2">
-              <h4 className="text-white">{project.year}</h4>
+              <h4 className="text-white">{post.year}</h4>
             </div>
             <div className="transition-all ease duration-500 opacity-100 content text-center group-hover/tes:opacity-0 z-10">
-              <h1 className="text-3xl font-bold mb-3">{project.title}</h1>
+              <h1 className="text-3xl font-bold mb-3">{post.title}</h1>
               <p>
-                {project.desc[0].length > 125
-                  ? `${project.desc[0].slice(0, 125)}...`
-                  : project.desc[0]}
+                {post.excerpt[0].length > 125
+                  ? `${post.excerpt[0].slice(0, 125)}...`
+                  : post.excerpt[0]}
               </p>
               <div className="flex justify-center items-center flex-row mt-5 flex-wrap">
-                {project.tech.map((t, index) => (
+                {post.tech.map((t, index) => (
                   <span
                     key={index}
                     className="m-1 px-4 py-2 bg-gray-600 text-white "
@@ -60,9 +66,9 @@ export default function ProjectCard({ project, index, activeCategory }) {
   );
 }
 
-ProjectCard.propTypes = {
-  project: PropTypes.object.isRequired,
+postCard.propTypes = {
+  post: PropTypes.object.isRequired,
   index: PropTypes.number.isRequired,
-  activeCategory: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  activeStatus: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
     .isRequired,
 };
--- app/blog/post-card)/index.jsx
+++ app/blog/post-card)/index.jsx
@@ -4,11 +4,11 @@ import { motion } from "framer-motion";
 import PropTypes from "prop-types";
 import BlurImage from "@/public/image/placeholder/blur.jpg";
 
-export default function ProjectCard({ project, index, activeCategory }) {
+export default function PostCard({ post, index, activeStatus }) {
   return (
     <>
-      {project.category.includes(parseInt(activeCategory)) && (
-        <Link href={"projects/" + project.slug} key={index}>
+      {post.status.includes(parseInt(activeStatus)) && (
+        <Link href={"blog/" + blog.slug} key={index}>
           <motion.div
             className="z-10 relative flex justify-center items-start flex-col mb-5 md:px-10 w-full h-auto bg-gray-400 group/tes py-20 px-5 md:py-2 aspect-video "
             initial={{
@@ -24,8 +24,8 @@ export default function ProjectCard({ project, index, activeCategory }) {
             }}
           >
             <Image
-              src={project.thumbnail}
-              alt="Mihalhayail"
+              src={post.thumbnail}
+              alt="Artikel"
               layout="fill"
               objectFit="cover"
               placeholder="blur"
@@ -33,17 +33,17 @@ export default function ProjectCard({ project, index, activeCategory }) {
               blurDataURL={BlurImage.src}
             />
             <div className="absolute top-0 left-0 bg-gray-600 px-4 py-2">
-              <h4 className="text-white">{project.year}</h4>
+              <h4 className="text-white">{post.year}</h4>
             </div>
-            <div className="transition-all ease duration-500 opacity-100 content text-center group-hover/tes:opacity-0 z-10">
-              <h1 className="text-3xl font-bold mb-3">{project.title}</h1>
+            <div className="transition-all ease duration-500 opacity-100 post text-center group-hover/tes:opacity-0 z-10">
+              <h1 className="text-3xl font-bold mb-3">{post.title}</h1>
               <p>
-                {project.desc[0].length > 125
-                  ? `${project.desc[0].slice(0, 125)}...`
-                  : project.desc[0]}
+                {post.excerpt[0].length > 125
+                  ? `${post.excerpt[0].slice(0, 125)}...`
+                  : post.excerpt[0]}
               </p>
               <div className="flex justify-center items-center flex-row mt-5 flex-wrap">
-                {project.tech.map((t, index) => (
+                {post.categories.map((t, index) => (
                   <span
                     key={index}
                     className="m-1 px-4 py-2 bg-gray-600 text-white "
@@ -60,9 +60,9 @@ export default function ProjectCard({ project, index, activeCategory }) {
   );
 }
 
-ProjectCard.propTypes = {
-  project: PropTypes.object.isRequired,
+PostCard.propTypes = {
+  post: PropTypes.object.isRequired,
   index: PropTypes.number.isRequired,
-  activeCategory: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  activeStatus: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
     .isRequired,
 };
