22c04a7da1e2175d1959cd7459d9bcbd389b62cf
Mihalhayail <mihalhayail85@gmail.com>
2025-08-26 03:36:52 +0700

    add blog every place

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -167,6 +167,21 @@ const NavItems = ({ isNavOpen, setIsNavOpen }) => {
                 </motion.h2>
               </div>
             </Link>
+            <Link href="/blog">
+              <div
+                onClick={handleItemClick}
+                className="text-2xl font-bold text-white"
+              >
+                <motion.h2
+                  className="text-white"
+                  variants={itemVariants}
+                  animate={isNavOpen ? "open" : "closed"}
+                  custom={0.3}
+                >
+                  Blog
+                </motion.h2>
+              </div>
+            </Link>
             <Link href="/#contact">
               <div
                 onClick={handleItemClick}
